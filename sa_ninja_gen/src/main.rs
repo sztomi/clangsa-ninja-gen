bcd316b3d518ebc93595d6041339192a9c60e3a7
Tamás Szelei <szelei.t@gmail.com>
2023-04-23 19:57:42 +0200

    sa_ninja_gen: somewhat less lousy error handling

--- sa_ninja_gen/src/main.rs
+++ sa_ninja_gen/src/main.rs
@@ -3,11 +3,13 @@ mod types;
 mod utils;
 mod ninjagen;
 
+use anyhow::Result;
 use clap::Parser;
 use cli::Opts;
 
-fn main() {
+fn main() -> Result<()> {
   let opts: Opts = Opts::parse();
-  let mut ng = ninjagen::NinjaGen::new(opts.into()).unwrap();
-  ng.generate().unwrap();
+  let mut ng = ninjagen::NinjaGen::new(opts.into())?;
+  ng.generate()?;
+  Ok(())
 }
